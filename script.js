207b02176ef1207c1751ad3cfe3494d7317253e0
boldPro <probold777@gmail.com>
2023-01-06 21:21:08 -0500

    added slider working/ but laggin

---
+++ script.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper('.swiper-container', {
+    direction: 'vertical',
+    sliderPerView: 2,
+    spaceBetween: 0,
+    mousewheel: false,
+    pagination: {
+        el:'.swiper-pagination',
+        type: 'progressbar',
+    }
+}) 
+
