226884602c70536b05ad87b74a91f11fcf8e746a
boldPro <probold777@gmail.com>
2023-01-11 21:12:53 -0500

    Email me working

--- script.js
+++ script.js
@@ -3,21 +3,51 @@ let mainSwiper = new Swiper('.parent-slider', {
     sliderPerView: 1,
     spaceBetween: 0,
     mousewheel: true,
+    effect: 'creative',
+  creativeEffect: {
+    prev: {
+      // will set `translateZ(-400px)` on previous slides
+      translate: [0, 0, -400],
+    },
+    next: {
+      // will set `translateX(100%)` on next slides
+      translate: ['100%', 0, 0],
+    },
+  },
     pagination: {
         el:'.swiper-pagination',
         type: 'progressbar',
     }
 }) 
 
-new Swiper(".project-slider", {
-    direction: 'horizontal',
-    loop: true,
-    slidesPerView: 1,
-    noSwiping: false,
-    pagination: '.swiper-pagination-child',
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-});
 
+AOS.init();
+
+// You can also pass an optional settings object
+// below listed default settings
+AOS.init({
+  duration: 1200
+})
+
+
+
+const btn = document.getElementById('email-form-button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'service_ryfc24s';
+   const templateID = 'template_g9yxsbq';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
