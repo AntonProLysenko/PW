b444dd19373b31a737ba6cefa26cda75480dc21a
boldPro <probold777@gmail.com>
2024-02-12 10:35:50 -0500

    Prevented dragging while scrolling verticaly 4

--- script.js
+++ script.js
@@ -187,7 +187,7 @@ function dragElement(elmnt) {
       pos2 = pos4 - e.touches[0].clientY;
       pos4 = e.touches[0].clientY;
       console.log('YSCROLL', pos4);
-      if (pos2 <=-1.5 && pos2 >=1.5){
+      if (pos2 <=-2 && pos2 >=2){
         pos1 = 0
         // pos3 = null
       }else{
