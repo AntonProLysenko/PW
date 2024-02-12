8a3556bbedcd13c3284c77d80af04fd707c0d4be
boldPro <probold777@gmail.com>
2024-02-12 09:01:25 -0500

    touch controllers for dragging

--- script.js
+++ script.js
@@ -158,44 +158,51 @@ function dragElement(elmnt) {
     console.dir(elmnt)
     /* if present, the header is where you move the DIV from:*/
     elmnt.addEventListener("mousedown", dragMouseDown)
-    
+    elmnt.addEventListener("touchstart", dragMouseDown)
+    // elmnt.ontouchstart = dragMouseDown;
   } else {
     /* otherwise, move the DIV from anywhere inside the DIV:*/
     elmnt.onmousedown = dragMouseDown;
+    elmnt.ontouchstart = dragMouseDown;
   }
 
   function dragMouseDown(e) {
 
-    console.log("dragging");
+    console.log("dragg Started");
 
     e = e || window.event;
     e.preventDefault();
     // get the mouse cursor position at startup:
     pos3 = e.clientX;
-    // pos4 = e.clientY;
+    pos4 = e.clientY;
     document.onmouseup = closeDragElement;
+    document.ontouchend = closeDragElement;
     // call a function whenever the cursor moves:
     document.onmousemove = elementDrag;
+    document.ontouchmove = elementDrag
   }
 
   function elementDrag(e) {
-    console.log("elem_dtag")
+    console.log("dragging")
     e = e || window.event;
     e.preventDefault();
     // calculate the new cursor position:
     pos1 = pos3 - e.clientX;
-    // pos2 = pos4 - e.clientY;
+    pos2 = pos4 - e.clientY;
     pos3 = e.clientX;
-    // pos4 = e.clientY;
+    pos4 = e.clientY;
     // set the element's new position:
     elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
     elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
   }
 
   function closeDragElement() {
+    console.log("Drag Ended")
     /* stop moving when mouse button is released:*/
     document.onmouseup = null;
+    document.ontouchend = null;
     document.onmousemove = null;
+    document.ontouchmove = null;
   }
 }
 
